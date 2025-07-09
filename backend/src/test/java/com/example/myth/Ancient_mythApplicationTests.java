d716ae68e8d76ec6b5bc8a3524e85baa3b8225a8
231250166 <@dsfdsaf>
2025-07-09 18:47:58 +0800

    修改后端

--- backend/src/test/java/com/example/myth/Ancient_mythApplicationTests.java
+++ backend/src/test/java/com/example/myth/Ancient_mythApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TomatoMallApplicationTests {
+class Ancient_mythApplicationTests {
 
     @Test
     void contextLoads() {
